18e8261932cc9fc4ce9b888f30894d4e419f4e4b
冉文杰 <ranwenjie@didichuxing.com>
2016-03-03 12:41:16 +0800

    first ci

--- KeyWindowDemo/AppDelegate.m
+++ KeyWindowDemo/AppDelegate.m
@@ -7,9 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
@@ -17,6 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[ViewController alloc] init];
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
--- KeyWindowDemo/ViewController.m
+++ KeyWindowDemo/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
-
+@property(nonatomic,strong) UIWindow *adWindow;
 @end
 
 @implementation ViewController
@@ -17,6 +18,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    // add a textfield
+    UITextField *filed = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 320, 60)];
+    filed.placeholder = @"Input something here";
+    filed.clearsOnBeginEditing = YES;
+    filed.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:filed];
+    
+    
+
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    UIWindow *window1 = [[UIWindow alloc] initWithFrame:CGRectMake(0, 80, 320, 320)];
+    window1.backgroundColor = [UIColor redColor];
+    
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor redColor];
+    window1.rootViewController = vc;
+    [window1 makeKeyAndVisible];
+    window1.windowLevel = UIWindowLevelNormal;
+    self.adWindow = window1;// window1 没有被强应用就释放了
+    
+    NSLog(@"%@",[UIApplication sharedApplication].windows);
+    NSLog(@"%@",[UIApplication sharedApplication].keyWindow);
+    NSLog(@"%@",((AppDelegate *)[UIApplication sharedApplication].delegate).window);
+    
+    
+    //[UIApplication sharedApplication].delegate.window.alpha = 0;
+//    [UIView animateWithDuration:5 animations:^{
+//        self.adWindow.alpha = 0.f;
+//    } completion:^(BOOL finished) {
+//        [((AppDelegate *)[UIApplication sharedApplication].delegate).window makeKeyWindow];
+//        self.adWindow.hidden = YES;
+//        [self.adWindow resignKeyWindow];
+//    }];
+
 }
 
 - (void)didReceiveMemoryWarning {
